e9a28d9c767b8b127d806ad042582d57da6cb906
muunseo <mkogo90@naver.com>
2024-10-16 22:30:52 +0900

    activity_intro.xml 업데이트

--- app/src/main/java/edu/sungshin/ecopath/MainActivity.kt
+++ app/src/main/java/edu/sungshin/ecopath/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.sungshin.ecopath
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sungshin.ecopath.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/edu/sungshin/ecopath/SearchidActivity.kt
@@ -0,0 +1,4 @@
+package edu.sungshin.ecopath
+
+class SearchidActivity {
+}
