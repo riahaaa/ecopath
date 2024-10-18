27da4f3d6e5dfc679a674c13779965275d18cb95
hozziii <nohh01089@gmail.com>
2024-10-18 21:09:22 +0900

    (로그인화면:로그아웃처리까쥐)

--- app/src/main/java/edu/sungshin/ecopath/IntroActivity.kt
+++ app/src/main/java/edu/sungshin/ecopath/IntroActivity.kt
@@ -23,16 +23,20 @@ class IntroActivity : AppCompatActivity() {
             // FirebaseAuth 인스턴스 가져오기
             val auth = FirebaseAuth.getInstance()
 
+            // 명시적으로 로그아웃 처리 (세션 초기화)
+            auth.signOut()
+
             // 로그인 상태에 따라 화면 전환
-            val intent = if(auth.currentUser != null){
+            val intent = if (auth.currentUser != null) {
                 // 사용자가 로그인되어 있으면 HomeActivity로 이동
                 Intent(this, HomeActivity::class.java)
-            }else{
+            } else {
                 // 로그인되지 않은 경우 LoginActivity로 이동
                 Intent(this, LoginActivity::class.java)
             }
             startActivity(intent)
             finish()
         }, 2000) // 2초 대기
+
     }
 }
