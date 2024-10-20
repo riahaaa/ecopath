cac615dd63b3eafd10db220acd403a1dc8deda55
heesgone <20231431@sungshin.ac.kr>
2024-10-20 18:48:11 +0900

    회원탈퇴 xml 변경

--- app/src/main/java/edu/sungshin/ecopath/CancelServiceActivity.kt
+++ app/src/main/java/edu/sungshin/ecopath/CancelServiceActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class CancelServiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cancel_service)
+        setContentView(R.layout.activity_deleteid)
 
     }
 }
